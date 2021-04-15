d26c389296f92fce773372e15c90d641fcfda5e0
kinfi4 <ilyamakarov223@gmail.com>
2021-04-15 17:58:29 +0300

    basic models created, maybe in future will change them

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from api.models import Game, KinGamesUser, CartGame, Cart, Category, Comment
 
-# Register your models here.
+
+admin.site.register(Game)
+admin.site.register(KinGamesUser)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(CartGame)
+admin.site.register(Comment)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class KinGamesUser(models.Model):
+    django_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='kin_user')
+    avatar = models.ImageField(upload_to='user_avatars', default='user_avatars/default_avatar.png')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=55)
+    slug = models.SlugField(unique=True)
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    preview_image = models.ImageField(upload_to='games_previews', default='games_previews/default.png')
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    slug = models.SlugField(unique=True)
+
+    categories = models.ManyToManyField(Category, related_name='games')
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(KinGamesUser, on_delete=models.CASCADE, related_name='comments')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='comments')
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(KinGamesUser, null=True, on_delete=models.CASCADE, related_name='carts')
+    user_agent = models.CharField(max_length=200, null=True, blank=True)
+    for_anonymous_user = models.BooleanField(default=False)
+
+    games = models.ManyToManyField(Game, related_name='related_carts', blank=True)
+    total_products = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=10, default=0, decimal_places=2)
+
+
+class CartGame(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default=1, verbose_name='Number of products')
+    final_price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.game.title
+
+    def save(self, *args, **kwargs):
+        self.final_price = self.qty * self.game.price
+        super().save(*args, **kwargs)
