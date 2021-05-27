26d39bcbb56938404bdea63eb8cc970dd5801cc1
Ubuntu <ubuntu@ip-172-31-34-126.eu-central-1.compute.internal>
2021-05-27 12:20:21 +0000

    some glitches with nginx fixed

--- KinGames/frontend/src/config.js
+++ KinGames/frontend/src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:8000/'
+export const BASE_URL = 'http://ec2-52-59-230-61.eu-central-1.compute.amazonaws.com/'
 export const ADMIN = 'ADMIN'
 export const MANAGER = 'MANAGER'
 export const USER = 'USER'
